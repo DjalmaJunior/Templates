73321d5013498711527bbf0c11efc2b496a95bab
Djalma Junior <djalmajunior03@outlook.com>
2022-11-23 15:19:26 -0300

    add token in header's login response

--- api/src/modules/public/login/router.ts
+++ api/src/modules/public/login/router.ts
@@ -7,6 +7,8 @@ const router = Router();
 router.get('/', async (req: QueryRequest<IParamsControllerLogin>, res) => {
   const data = await LoginController.login(req.query);
 
+  res.setHeader('token', data.token as string);
+
   res.send(data);
 });
 
