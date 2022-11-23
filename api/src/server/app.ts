f6f8bcf95a2f327d23b1a84629b53700d0b516d6
Djalma Junior <djalmajunior03@outlook.com>
2022-11-23 15:20:22 -0300

    change instance of express to singleton's class App

--- api/src/server/app.ts
+++ api/src/server/app.ts
@@ -1,9 +1,20 @@
 import 'express-async-errors';
-import express from 'express'
+import express, { Express } from 'express'
 import setupMiddlewares from './middlewares'
 
-const app = express()
+export default class App {
+  private static instance: Express;
 
-setupMiddlewares(app)
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
 
-export default app
+  public static getApp() {
+    if (!App.instance) {
+      App.instance = express();
+      
+      setupMiddlewares(App.instance);
+    }
+
+    return App.instance;
+  }
+}
--- api/src/server/index.ts
+++ api/src/server/index.ts
@@ -1,7 +1,8 @@
-import app from './app'
+import App from './app'
 import config from './config';
 
 const host = config.HOST || 'localhost';
 const port = config.PORT || 3000;
+const app = App.getApp();
 
 app.listen(port, () => console.log(`Server running at http://${host}:${port}`));
