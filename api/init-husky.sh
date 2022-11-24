8e1bebeff750cd84c5ee71a7d4a731bd79fabe69
Djalma Junior <djalmajunior03@outlook.com>
2022-11-24 09:19:29 -0300

    fix sh to create correct husky hook

--- api/init-husky.sh
+++ api/init-husky.sh
@@ -1,2 +1,2 @@
 # Must be run in the same directory as .git
-yarn add husky -D && cd .. && npx husky install api/.husky && npx husky add .husky/pre-commit "yarn test && yarn lint"
+yarn add husky -D && cd .. && npx husky install api/.husky && npx husky add api/.husky/pre-commit "cd api && yarn test-husky && yarn lint"
